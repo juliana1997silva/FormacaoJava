f7f0f505bdac328f4c00170caa10aedb3ff4ac1e
Juliana <julianajesus06@hotmail.com>
2025-01-03 19:22:41 -0300

    commit teste

--- primeiroprograma/src/Main.java
+++ primeiroprograma/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("juliana silva");
+        System.out.println("Juliana");
+        System.out.println("Juliana");
+        System.out.println("Juliana");
+        System.out.println("Juliana");
     }
 }
