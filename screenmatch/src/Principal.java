a0422fb9acd99709b9c0a4257977f20ac4d7fb6f
Juliana <julianajesus06@hotmail.com>
2025-01-06 09:13:32 -0300

    aplicando comportamento em comum

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,4 +1,6 @@
 import br.com.alura.screenmatch.calculo.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculo.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodios;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
@@ -41,6 +43,17 @@ public class Principal {
         calculadora.inclui(outraFilme);
         calculadora.inclui(lost);
         System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+
+        filtro.filtrar(meuFilme);
+
+        Episodios episodio = new Episodios();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacao(300);
+
+        filtro.filtrar(episodio);
 
     }
 }
